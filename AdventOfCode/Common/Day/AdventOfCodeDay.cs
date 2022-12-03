3db866fdff008b2739f5f8c1eaf40df5e592f452
AlmantasK <almantusk@gmail.com>
2022-12-03 12:17:26 +0200

    test: duplicate Days input

--- AdventOfCode/Common/Day/AdventOfCodeDay.cs
+++ AdventOfCode/Common/Day/AdventOfCodeDay.cs
@@ -13,7 +13,7 @@
 
         public void Solve()
         {
-            var input = ReadInputInAHackyWayWorkingForTests();
+            var input = File.ReadAllText(@$"Days\{Day}.txt");
 
 
             var part1 = new TPart1();
@@ -21,27 +21,6 @@
 
             Console.WriteLine($"Day{Day}, part1 answer: " + part1.Solve(input));
             Console.WriteLine($"Day{Day}, Part2 answer: " + part2.Solve(input));
-        }
-
-        /// <summary>
-        /// Some tests runners copy the files from the bin of app under test, other don't.
-        /// This is a workaround to cater for both scenarios.
-        /// </summary>
-        private string ReadInputInAHackyWayWorkingForTests()
-        {
-            string input;
-            try
-            {
-                Directory.SetCurrentDirectory(@".\..\..\..\..\AdventOfCode.Exec\bin\Debug\net6.0");
-                input = File.ReadAllText(@$"Days\{Day}.txt");
-            }
-            catch
-            {
-                Directory.SetCurrentDirectory(@".\..\..\..\..\AdventOfCode.Tests\bin\Debug\net6.0");
-                input = File.ReadAllText(@$"Days\{Day}.txt");
-            }
-
-            return input;
         }
     }
 }
