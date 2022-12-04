06a005183ef6e426dcb95f52d44cf5b43581dec8
AlmantasK <almantusk@gmail.com>
2022-12-04 12:23:37 +0200

    feat: determine whether 2 ranges fully overlap

---
+++ AdventOfCode.Tests/Day4/SectionRangeTests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode.Day4;
+
+namespace AdventOfCode.Tests.Day4;
+
+public class SectionRangeTests
+{
+    [Theory]
+    [InlineData(1, 1, 1, 1, true)]
+    [InlineData(1, 2, 1, 1, true)]
+    [InlineData(2, 2, 1, 1, false)]
+    [InlineData(1, 1, 1, 2, true)]
+    [InlineData(1, 2, 2, 3, false)]
+    public void IsFullOverlap_Returns_Whether_EitherRange_CanFitFullyIntoOneAnother(int range1Start, int range1End, int range2Start, int range2End, bool expectedIsFullOverlap)
+    {
+        var range1 = new SectionRange(range1Start, range1End);
+        var range2 = new SectionRange(range2Start, range2End);
+
+        var isFullOverlap = range1.IsFullOverlapWith(range2);
+
+        isFullOverlap.Should().Be(expectedIsFullOverlap);
+    }
+}
---
+++ AdventOfCode/Day4/SectionRange.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.Day4;
+
+public struct SectionRange
+{
+    public int IdFrom { get; }
+    public int IdTo { get; }
+
+    public SectionRange(int idFrom, int idTo)
+    {
+        IdFrom = idFrom;
+        IdTo = idTo;
+    }
+
+    public bool IsFullOverlapWith(SectionRange other)
+    {
+        SectionRange biggerRange;
+        SectionRange rangeToFit;
+
+        // Bigger range starts at the same place or earlier.
+        if (this.IdFrom < other.IdFrom)
+        {
+            biggerRange = this;
+            rangeToFit = other;
+        }
+        // In case both ranges start at the same place, the bigger one is the one that ends later.
+        else if (this.IdFrom == other.IdFrom && this.IdTo >= other.IdTo)
+        {
+            biggerRange = this;
+            rangeToFit = other;
+        }
+        else
+        {
+            biggerRange = other;
+            rangeToFit = this;
+        }
+
+        return rangeToFit.IdFrom >= biggerRange.IdFrom && rangeToFit.IdTo <= biggerRange.IdTo;
+    }
+}
