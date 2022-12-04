a2beea3060b82329646f021be58427ae905266ca
AlmantasK <almantusk@gmail.com>
2022-12-04 12:23:00 +0200

    chore: day 4 prep

---
+++ AdventOfCode.Tests/Day4/Day4Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode.Day4;
+
+namespace AdventOfCode.Tests.Day4
+{
+    public class Day4Tests
+    {
+        // Line = 2 ranges of section ids
+        // Ranges overlap
+        // How many ranges fully contain each other?
+
+        [Theory]
+        [MemberData(nameof(Day1Part1MaxSumOfCalories))]
+        public void Part1_Solve_Returns_MaxSumOfCalories(string calories, long expectedMax)
+        {
+            var part1 = new Part1();
+
+            var measureIncreases = part1.Solve(calories);
+
+            measureIncreases.Should().Be(expectedMax);
+        }
+
+        [Theory]
+        [MemberData(nameof(Day1Part2ExpectedSumOfTop3SumsOfCalories))]
+        public void Part2_Solve_Returns_IncreasesCountOverAWindowOf3(string calories, long expectedTop3Max)
+        {
+            var part2 = new Part2();
+
+            var measureIncreases = part2.Solve(calories);
+
+            measureIncreases.Should().Be(expectedTop3Max);
+        }
+
+        public static IEnumerable<object[]> Day1Part1MaxSumOfCalories
+        {
+            get
+            {
+                yield return Expect(day: 1, file: "Example", result: 2);
+            }
+        }
+
+        public static IEnumerable<object[]> Day1Part2ExpectedSumOfTop3SumsOfCalories
+        {
+            get
+            {
+                yield return Expect(day: 1, file: "Example", result: 45000);
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day4/Solution.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Common.Day;
+using AdventOfCode.Common.Extensions;
+
+namespace AdventOfCode.Day4
+{
+    public class Solution : AdventOfCodeDay<Part1, Part2>
+    {
+        protected override int Day => 1;
+    }
+
+    public class Part1 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+
+    public class Part2 : IPartSolution
+    {
+        public long Solve(string input)
+        {
+            return 0;
+        }
+    }
+}
